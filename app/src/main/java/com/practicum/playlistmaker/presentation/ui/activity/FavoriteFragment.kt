438a37c0b6ff743312cd110f40456a2dadf88e39
jimma <rumuzi1991@gmail.com>
2024-07-29 12:13:04 +0300

    Спринт 18. Выполнение задания.

---
+++ app/src/main/java/com/practicum/playlistmaker/presentation/ui/activity/FavoriteFragment.kt
@@ -0,0 +1,25 @@
+package com.practicum.playlistmaker.presentation.ui.activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.practicum.playlistmaker.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment : Fragment() {
+    var _binding : FragmentFavoriteBinding? = null
+    val binding get() = _binding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    companion object {
+        fun newInstance() = FavoriteFragment()
+
+    }
+}
